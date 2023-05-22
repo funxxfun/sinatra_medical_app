aafac7913ae19a6fcae6b21afdf270886c43436c
funxxfun <23shirasawa@gmail.com>
2023-05-23 07:29:09 +0900

    投稿編集ページとルーティングを追加

--- sinatra_blog_app/app.rb
+++ sinatra_blog_app/app.rb
@@ -1,9 +1,9 @@
-require "sinatra"
-require "sinatra/activerecord"
-require "sinatra/reloader"
-require "./models.rb"
+require 'sinatra'
+require 'sinatra/activerecord'
+require 'sinatra/reloader'
+require './models.rb'
 
-set :database, {adapter: "sqlite3", database: "myblogdb.sqlite3"}
+set :database, {adapter: 'sqlite3', database: 'myblogdb.sqlite3'}
 
 get '/' do
   @posts = Post.all
@@ -18,4 +18,11 @@ end
 get '/post/:id' do
   @post = Post.find(params[:id])
   erb :post_page
+end
+
+put '/post/:id' do
+  @post = Post.find(params[:id])
+  @post.update(title: params[:title], body: params[:body])
+  @post.save
+  redirect '/post/'+params[:id]
 end
