2cfa6c655bc5e08ac79b0c8741bbdcc0e5d5cb8b
funxxfun <23shirasawa@gmail.com>
2023-06-09 06:24:37 +0900

    sessionを使用してログイン機能を実装した

--- sinatra_blog_app/models.rb
+++ sinatra_blog_app/models.rb
@@ -7,4 +7,9 @@ class User < ActiveRecord::Base
   validates_presence_of :name
   validates_length_of :password, :maxmum => 10, :minimum => 4
   has_many :posts
+
+  # def authenticate(user_id)
+  #   return self.id == user_id ? true : false
+  # end
+
 end
